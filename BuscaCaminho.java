f87677da869fac423fb73e15c9de13b9a158b330
Arthur Fenilli <arthurfenillidesouza@gmail.com>
2024-06-11 18:17:32 -0300

    initial commit.

---
+++ BuscaCaminho.java
@@ -0,0 +1,38 @@
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class BuscaCaminho {
+
+    private double caminhoVal;
+    private ArrayList<Cidade> caminho;
+
+    public BuscaCaminho(){
+        caminho = new ArrayList<>();
+    }
+
+    private void  leituraDeArquivo(){
+        Path path =
+    }
+
+
+
+    class Cidade{
+        private String nome;
+        private double latitude;
+        private double longitude;
+    
+    
+        public Cidade(String nome, double latitude, double longitude){
+            this.nome = nome;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+
+
+
+    }
+
+
+
+}
