77544178549f353b4e68e5f8aae7baa2cdedebfd
ArthurFenilli <dincaradadooeste@gmail.com>
2024-06-15 15:27:26 -0300

    basic problem structures implemented.

--- BuscaCaminho.java
+++ BuscaCaminho.java
@@ -3,15 +3,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 public class BuscaCaminho {
 
-    private double caminhoVal;
-    private ArrayList<Cidade> cidades;
+    private LinkedList<Cidade> cidades;
+    private double melhor_caminho;
 
     public BuscaCaminho(){
-        cidades = new ArrayList<>();
+        cidades = new LinkedList<>();
+        melhor_caminho = Double.MAX_VALUE;
     }
 
     private void  leituraDeArquivo(String caminho){
@@ -40,9 +43,36 @@ public class BuscaCaminho {
     }
 
     public void tracaCaminho(){
-        while(true){
+        int count = 0;
+        LinkedList<Cidade> cit_2 = new LinkedList<>(cidades);
+        LinkedList<Cidade> cit_3 = new LinkedList<>(cidades);
+        Collections.shuffle(cit_2);
+        Collections.shuffle(cit_3);
+        Caminho c_1 = new Caminho(cidades);
+        Caminho c_2 = new Caminho(cit_2);
+        Caminho c_3 = new Caminho(cit_3);
+        System.out.println("Caminho 1:" + c_1.caminhoVal);
+        System.out.println("Caminho 2:" + c_2.caminhoVal);
+        System.out.println("Caminho 3:" + c_3.caminhoVal);
+        ArrayList<Caminho> selec = new ArrayList<>();
+        selec.add(c_1);
+        selec.add(c_2);
+        selec.add(c_3);
 
+        while (true) {
+
+            Collections.sort(selec);
+
+            if(melhor_caminho > selec.get(0).caminhoVal){
+                melhor_caminho = selec.get(0).caminhoVal;
+            }
+            if(count %500 == 0){
+                System.out.println("Iterações:" + count + " | Melhor caminho:" + melhor_caminho);
+            }
+
+            count++;
         }
+
     }
 
 
@@ -63,9 +93,62 @@ public class BuscaCaminho {
             return nome;
         }
 
+        public double getLatitude(){
+            return latitude;
+        }
+
+        public double getLongitude(){
+            return longitude;
+        }
 
 
+    }
 
+    class Caminho implements Comparable<Caminho>{
+        private double caminhoVal;
+        private LinkedList<Cidade> caminho; 
+
+        public Caminho(LinkedList<Cidade> caminho){
+            this.caminho = caminho;
+            caminhoVal = CalculaCaminho();
+            
+        }
+
+        private double CalculaCaminho(){
+            double lat_prev = -1;
+            double long_prev = -1;
+            double lat_cur = 0;
+            double long_cur = 0;
+            double dist = 0;
+
+            for(Cidade c : caminho){
+                lat_cur = c.getLatitude();
+                long_cur = c.getLongitude();
+                if(lat_prev != -1){
+                    dist = dist + Math.sqrt(Math.pow(lat_cur - lat_prev, 2) + Math.pow(long_cur - long_prev, 2));
+                }
+                lat_prev = lat_cur;
+                long_prev = long_cur;
+            }
+
+            lat_cur = caminho.get(0).getLatitude();
+            long_cur = caminho.get(0).getLongitude();
+
+            dist = dist + Math.sqrt(Math.pow(lat_cur - lat_prev, 2) + Math.pow(long_cur - long_prev, 2));
+            return dist;
+        }
+
+    
+        @Override
+        public int compareTo(Caminho c) {
+            if(this.caminhoVal < c.caminhoVal){
+                return -1;
+            }
+            else if(this.caminhoVal > c.caminhoVal){
+                return 1;
+            }
+            return 0;
+        }
     }
 
     private void listarCidades(){
@@ -77,8 +160,7 @@ public class BuscaCaminho {
     public static void main(String args[]){
         BuscaCaminho b = new BuscaCaminho();
         b.leituraDeArquivo("data.txt");
-        //b.listarCidades();
-        System.out.println(b.cidades.size());
+        b.tracaCaminho();
     }
 
 
