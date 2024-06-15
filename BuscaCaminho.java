d1784df647c0971f7232ca56243b26e83707785c
Arthur Fenilli <arthurfenillidesouza@gmail.com>
2024-06-15 11:20:25 -0300

    reading method implemented.

--- BuscaCaminho.java
+++ BuscaCaminho.java
@@ -1,17 +1,48 @@
+import java.io.BufferedReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class BuscaCaminho {
 
     private double caminhoVal;
-    private ArrayList<Cidade> caminho;
+    private ArrayList<Cidade> cidades;
 
     public BuscaCaminho(){
-        caminho = new ArrayList<>();
+        cidades = new ArrayList<>();
     }
 
-    private void  leituraDeArquivo(){
-        Path path =
+    private void  leituraDeArquivo(String caminho){
+        Path path = Paths.get(caminho);
+
+        try(BufferedReader reader = Files.newBufferedReader(path)){
+            String line;
+
+            while((line = reader.readLine()) != null){
+                try {
+                    String aux[] = line.split(" ");
+                    double latitude = Double.parseDouble(aux[0]);
+                    double longitude = Double.parseDouble(aux[1]);
+                    String nome = aux[2];
+                    Cidade cidade = new Cidade(nome, latitude, longitude);
+                    cidades.add(cidade);
+
+                } catch (Exception e) {
+                    System.out.println("A");
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println("Erro ao ler o arquivo.");
+        }
+    }
+
+    public void tracaCaminho(){
+        while(true){
+
+        }
     }
 
 
@@ -28,10 +59,28 @@ public class BuscaCaminho {
             this.longitude = longitude;
         }
 
+        public String getNome(){
+            return nome;
+        }
+
 
 
 
     }
+
+    private void listarCidades(){
+        for(Cidade c: cidades){
+            System.out.println(c.getNome());
+        }
+    }
+
+    public static void main(String args[]){
+        BuscaCaminho b = new BuscaCaminho();
+        b.leituraDeArquivo("data.txt");
+        //b.listarCidades();
+        System.out.println(b.cidades.size());
+    }
+
 
 
 
